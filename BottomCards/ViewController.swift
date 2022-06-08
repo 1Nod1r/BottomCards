5ee2adb2b540b8f4945c222fa8f5557f37777601
1Nod1r <kh.nodirbeek@gmail.com>
2022-06-08 14:25:32 +0500

    Pet project

--- BottomCards/ViewController.swift
+++ BottomCards/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import BLTNBoard
 
 class ViewController: UIViewController {
-
+    
+    private lazy var boardManager: BLTNItemManager = {
+        let item = BLTNPageItem(title: "Push Notifications")
+        item.image = UIImage(systemName: "bell.fill")
+        item.actionButtonTitle = "Continue"
+        item.alternativeButtonTitle = "Maybe later"
+        item.descriptionText = "Would you like to get notifications from out company"
+        item.actionHandler = { _ in
+            self.didTapBoardButton()
+        }
+        item.alternativeHandler = { _ in
+            self.didTapSkip()
+        }
+        
+        return BLTNItemManager(rootItem: item)
+    }()
+    
+    @IBOutlet var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myButton.backgroundColor = .link
+        myButton.tintColor = .white
+    }
+    
+    @IBAction func didTapButton() {
+        boardManager.showBulletin(above: self)
     }
 
+    func didTapBoardButton(){
+        print("Did Tap continue")
+    }
+    
+     func didTapSkip(){
+        boardManager.dismissBulletin(animated: true)
+        print("Did tap Skip")
+    }
 
 }
 
